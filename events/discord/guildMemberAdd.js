e42c14156cd683e59f4ba234d9a152c8a104d20c
WilliamAcosta1 <williamacosta00814@gmail.com>
2022-05-20 21:01:05 -0500

    changes

--- events/discord/guildMemberAdd.js
+++ events/discord/guildMemberAdd.js
@@ -12,7 +12,7 @@ module.exports = {
         let embed = new Discord.MessageEmbed()
 
             .setColor(botConfig.embeds.color)
-            .setDescription(`> **<@${member.user.id}> ha llegado a Latinoland!** 🎉\n Bienvenido a la comunidad de Latinoland en Discord!\n\n💻 Pasa el rato e interactúa con otros miembros de la comunidad!\n\n\`\`\`Sé respetuoso con los demas miembros de la comunidad y no olvides leer y respetar nuestras reglas. 📝 \`\`\`\n\n**Miembros totales »** ${client.guilds.cache.get(botConfig.serverId).memberCount}\n**Página web »** https://web.latinoland.net/`)
+            .setDescription(`> **<@${member.user.id}> ha llegado a Latinoland!** 🎉\n Bienvenido a la comunidad de Latinoland en Discord!\n\n💻 Pasa el rato e interactúa con otros miembros de la comunidad!\n\n\`\`\`Sé respetuoso con los demas miembros de la comunidad y no olvides leer y respetar nuestras reglas. 📝 \`\`\`\n**Miembros totales »** ${client.guilds.cache.get(botConfig.serverId).memberCount}\n**Página web »** https://web.latinoland.net/`)
             .setThumbnail(member.user.avatarURL({ dynamic: true }))
             .setTimestamp()
 
