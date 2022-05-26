c4f4b57fa080712e2a6304a7d659e60ab7de5879
WilliamAcosta1 <williamacosta00814@gmail.com>
2022-05-25 22:55:04 -0500

    changes

--- commands/Utils/ip.js
+++ commands/Utils/ip.js
@@ -18,7 +18,7 @@ module.exports = {
 		const embed = new Discord.MessageEmbed()
 
 			.setAuthor({ name: 'LatinoLand', iconURL: botConfig.media.botStatusIMG })
-			.setDescription("Aqui te dejo la ip de java y bedrock. <a:irongolem:979227313801670696> \n\n**NOTA:** El puerto es por defecto asi que no lo modifiques.\n⠀⠀")
+			.setDescription("Aqui te dejo la ip del servidor. <a:irongolem:979227313801670696> \n\n**NOTA:** El puerto es por defecto asi que no lo modifiques.\n⠀⠀")
 			.addField('<:java:979227213754949712> Java edition', '**┕ **' + '`latinoland.net`', true)
 			.addField('<:bedrock:979227059022884924> Bedrock edition', '**┕ **' + '`latinoland.net`', true)
 			.addField('<a:MC_XP:979230021954723880> Estado:', '**┕ **' + '`Cerrado teporalmene`')
