d587c69fdcc2ad825bab88c706bdb2a6e76d502b
WilliamAcosta1 <williamacosta00814@gmail.com>
2022-06-02 20:29:36 -0500

    changes

--- commands/Info/userinfo.js
+++ commands/Info/userinfo.js
@@ -1,12 +1,7 @@
 // init require
 const Discord = require('discord.js');
 const botConfig = require("@config/bot.js");
-const { default: localizify, t } = require('localizify');
-var moment = require('moment');
-const fs = require('fs');
 var { image_search } = require('duckduckgo-images-api');
-var remote = require('remote-file-size')
-const { getBotVersion, getFooter, getTulipoSettings, setTulipoSettings } = require("@modules/utils/data.js");
 const emoji = require('@config/emojis.js');
 require("@modules/localization.js");
 
@@ -22,8 +17,10 @@ module.exports = {
 	nsfw: false,
 	async execute(client, message, args) {
 
+
 		let user = message.mentions.users.first() || message.author;
 		let member = message.mentions.members.first() || message.member;
+
 
 		let game;
 		if (member.presence.activities[1] != undefined) {
@@ -36,18 +33,16 @@ module.exports = {
 
 		const embed = new Discord.MessageEmbed()
 
-			.setTitle('command.userinfo.title'.trans()) //emoji.info + ' ' + 
+			.setTitle('command.userinfo.title'.trans())
 			.setThumbnail(user.avatarURL({ dynamic: true }))
-			.setDescription('**' + 'command.userinfo.user'.trans() + ':** ' + `${user.username}` + '#' + `${user.discriminator}` + '\n**' + 'command.userinfo.nickname'.trans() + ':** ' + `${member.nickname || 'command.userinfo.no_nickname'.trans()}` + '\n**' + 'command.userinfo.id'.trans() + ':** ' + `${user.id}` + '\n**' + 'command.userinfo.color'.trans() + ':** ' + `${user.hexAccentColor}` + '\n**' + 'command.userinfo.playing'.trans() + ':** ' + game)
-			//.setImage(user.bannerURL({ dynamic: true })) // TODO: Hacerlo funcional el banner
-			//.addField('command.userinfo.creation'.trans() + ': ', 'Agregar el fecha')
-			//.addField('command.userinfo.login'.trans() + ': ', 'Fecha')
+			.setDescription('**' + 'command.userinfo.user'.trans() + ':** ' + `${user.username}` + '#' + `${user.discriminator}` + '\n**' + 'command.userinfo.nickname'.trans() + ':** ' + `${member.nickname || 'command.userinfo.no_nickname'.trans()}` + '\n**' + 'command.userinfo.id'.trans() + ':** ' + `${user.id}` + '\n**' + 'command.userinfo.color'.trans() + ':** ' + `${member.displayHexColor}` + '\n**' + 'command.userinfo.playing'.trans() + ':** ' + game)
+			.addField('command.userinfo.creation'.trans() + ': ', `<t:${Math.floor(member.user.createdAt / 1000)}:d>` + ' (' + `<t:${Math.floor(member.user.createdAt / 1000)}:R>` + ')')
+			.addField('command.userinfo.login'.trans() + ': ', `<t:${Math.floor(member.joinedAt / 1000)}:d>` + ' (' + `<t:${Math.floor(member.joinedAt / 1000)}:R>` + ')')
 			.addField('command.userinfo.roles'.trans() + ': ', `${member.roles.cache.map(m => m).join(" • ")}`) //TODO: agregar mensaje Sin roles cuando no tenga ningun rol
 			.setColor(botConfig.embeds.color)
 			.setFooter({ text: getFooter(message.member.user.tag), iconURL: message.author.avatarURL({ dynamic: true }) })
 
 		return message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
-
 	}
 
 
