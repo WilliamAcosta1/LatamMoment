eebb96f292a8a54799b7b3e8abe0236b0e12fe9c
WilliamAcosta1 <williamacosta00814@gmail.com>
2022-12-18 19:25:00 -0500

    update

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config: any = {
+    name: "Zumito", //Name of bot
+    supportServerURL: "https://zumito.ga/server", //Support server link
+
+    embeds: {
+        color: '#f982a2',
+    }
+}
